f0211fba803fb4ac223484cae5e6d643798ee742
Andrew D'Angelo <dangeloandrew@outlook.com>
2018-02-25 13:06:36 -0600

    input, etc

--- main.c
+++ main.c
@@ -8,10 +8,18 @@
 
 /* globals */
 
-OBJ_ATTR *obj_cross= &oam_mem[0];
-OBJ_ATTR *obj_disp= &oam_mem[1];
+OBJ_ATTR *obj_cross = &oam_mem[0];
+OBJ_ATTR *obj_disp = &oam_mem[1];
 
 BG_AFFINE bgaff;
+AFF_SRC_EX asx = {
+	32<<8, 64<<8,           // Map coords.
+	120, 80,                // Screen coords.
+	0x0100, 0x0100, 0       // Scales and angle.
+};
+
+const int DX = 256;
+FIXED ss = 0x0100;
 
 /* prototypes */
 
@@ -24,13 +32,13 @@ void init_map();
 /* implementations */
 
 void win_textbox(int bgnr, int left, int top, int right, int bottom, int bldy) {
-	REG_WIN0H= left<<8 | right;
-	REG_WIN0V=  top<<8 | bottom;
-	REG_WIN0CNT= WIN_ALL | WIN_BLD;
-	REG_WINOUTCNT= WIN_ALL;
+	REG_WIN0H = left<<8 | right;
+	REG_WIN0V =  top<<8 | bottom;
+	REG_WIN0CNT = WIN_ALL | WIN_BLD;
+	REG_WINOUTCNT = WIN_ALL;
 
-	REG_BLDCNT= (BLD_ALL&~BIT(bgnr)) | BLD_BLACK;
-	REG_BLDY= bldy;
+	REG_BLDCNT = (BLD_ALL&~BIT(bgnr)) | BLD_BLACK;
+	REG_BLDY = bldy;
 
 	REG_DISPCNT |= DCNT_WIN0;
 
@@ -38,17 +46,17 @@ void win_textbox(int bgnr, int left, int top, int right, int bottom, int bldy) {
 }
 
 void init_cross() {
-	TILE cross= {{
+	TILE cross = {{
 		0x00011100, 0x00100010, 0x01022201, 0x01021201, 
 		0x01022201, 0x00100010, 0x00011100, 0x00000000,
 	}};
-	tile_mem[4][1]= cross;
+	tile_mem[4][1] = cross;
 
-	pal_obj_mem[0x01]= pal_obj_mem[0x12]= CLR_WHITE;
-	pal_obj_mem[0x02]= pal_obj_mem[0x11]= CLR_BLACK;
+	pal_obj_mem[0x01] = pal_obj_mem[0x12] = CLR_WHITE;
+	pal_obj_mem[0x02] = pal_obj_mem[0x11] = CLR_BLACK;
 
-	obj_cross->attr2= 0x0001;
-	obj_disp->attr2= 0x1001;
+	obj_cross->attr2 = 0x0001;
+	obj_disp->attr2 = 0x1001;
 }
 
 void init_map() {
@@ -64,6 +72,39 @@ void init_map() {
 
 void input_game() {
 	key_poll();
+
+	if(key_is_down(KEY_A)) {
+		/* dir + A : move map in screen coords */
+		asx.scr_x += key_tri_horz();
+		asx.scr_y += key_tri_vert();
+	} else {
+		/* dir : move map in map coords */
+		asx.tex_x -= DX*key_tri_horz();
+		asx.tex_y -= DX*key_tri_vert();
+	}
+
+	/* L / R : rotate */
+	asx.alpha -= 128*key_tri_shoulder();
+
+	/* B: scale up ; B+Se : scale down */
+	if(key_is_down(KEY_B))
+		ss += (key_is_down(KEY_SELECT) ? -1 : 1);
+
+	if(key_hit(KEY_START)) {
+		if(!key_is_down(KEY_SELECT)) {
+			/* St : toggle wrapping flag. */
+			REG_BG2CNT ^= BG_WRAP;
+		} else {
+			/* St+Se : reset */
+			asx.tex_x = asx.tex_y= 0;
+			asx.scr_x = asx.scr_y= 0;
+			asx.alpha = 0;
+			ss = 1<<8;
+		}
+	}
+
+	/* apply fixed point transformation */
+	asx.sx = asx.sy = (1<<16)/ss;
 }
 
 int main() {
@@ -71,7 +112,7 @@ int main() {
 	init_cross();
 
 	/* registers */
-	REG_DISPCNT= DCNT_MODE1 | DCNT_BG0 | DCNT_BG2 | DCNT_OBJ;
+	REG_DISPCNT = DCNT_MODE1 | DCNT_BG0 | DCNT_BG2 | DCNT_OBJ;
 
 	/* text system */
 	tte_init_chr4c_b4_default(0, BG_CBB(2)|BG_SBB(28));
@@ -86,6 +127,20 @@ int main() {
 	while(1) {
 		VBlankIntrWait();
 		input_game();
+
+		/* apply affine matrix to bg */
+		bg_rotscale_ex(&bgaff, &asx);
+		REG_BG_AFFINE[2] = bgaff;
+
+		/* update cross position (indicates rotation point)
+		   (== p in map-space; q in screen-space) */
+		obj_set_pos(obj_cross, asx.scr_x-3, (asx.scr_y-3));
+		obj_set_pos(obj_disp, (bgaff.dx>>8)-3, (bgaff.dy>>8)-3);
+
+		/* update hud */
+		tte_printf("#{es;P}p0\t: (%d, %d)\nq0\t: (%d, %d)\ndx\t: (%d, %d)", 
+		    asx.tex_x>>8, asx.tex_y>>8, asx.scr_x, asx.scr_y, 
+		    bgaff.dx>>8, bgaff.dy>>8);
 	}
 
 	return 0;
