e8d1c88cd7785d8d3507b8924c989ef70f5b832c
Andrew D'Angelo <dangeloandrew@outlook.com>
2018-04-18 13:08:40 -0500

    mode 7 sim

--- affine_sim.py
+++ affine_sim.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+import math
 
 # load map data
 map_data = np.array(Image.open('gfx/bc1floor.png').convert('RGB'))
@@ -19,25 +20,50 @@ class BGAffine:
         self.pb = 0
         self.pc = 0
         self.pd = 0
-        self.dx = 0
-        self.dy = 0
-    def m_P(self):
+        self.x = 0
+        self.y = 0
+    def P(self):
         return np.array([[self.pa, self.pb], [self.pc, self.pd]])
-    def v_dx(self):
-        return np.array([[self.dx], [self.dy]])
+    def dx(self):
+        return np.array([[self.x], [self.y]])
 bgaff = [BGAffine() for i in range(H + 1)]
+
+# mode 7 structures
+M7_D = 160.0
+class Vector:
+    def __init__(self, x=0.0, y=0.0, z=0.0):
+        self.x = x
+        self.y = y
+        self.z = z
+cam_pos = Vector(0.0, 32.0, 0.0)
+cam_phi = 0.0
 
 # calculate affine matrices
 for h in range(H):
-    bgaff[h].pa = 1.0 + (h / H)
-    bgaff[h].pd = 1.0 + (h / H)
+    if h == 0:
+        lam = 0.0
+    else:
+        lam = cam_pos.y / float(h)
+    lam_cos_phi = lam * math.cos(cam_phi)
+    lam_sin_phi = lam * math.sin(cam_phi)
+
+    bgaff[h].pa = lam_cos_phi
+    bgaff[h].pd = lam_sin_phi
+
+    lxr = 120.0 * lam_cos_phi
+    lyr = M7_D * lam_sin_phi
+    bgaff[h].x = cam_pos.x - lxr + lyr
+
+    lxr = 120.0 * lam_sin_phi
+    lyr = M7_D * lam_cos_phi
+    bgaff[h].y = cam_pos.z - lxr - lyr
 
 # apply affine transform
 for h in range(H):
     for i in range(W):
-        v_q = np.array([[i], [h]])
-        v_p = bgaff[h].v_dx() + bgaff[h].m_P() @ v_q
-        [px, py] = np.ndarray.tolist(np.ndarray.flatten(v_p))
+        q = np.array([[i], [h]])
+        p = bgaff[h].dx() + bgaff[h].P() @ q
+        [px, py] = np.ndarray.tolist(np.ndarray.flatten(p))
         screen_data[h, i] = map_data[int(px) % map_W, int(py) % map_H]
 
 # display screen data
