8b808630f588af733828b26dca31fdfb01c45504
Andrew D'Angelo <dangeloandrew@outlook.com>
2019-11-12 21:27:58 -0600

    different call sites for register assignment

--- sim/gba.cpp
+++ sim/gba.cpp
@@ -1,48 +1,201 @@
-#include <tonc.h>
-
-#include <assert.h>
-
-#include <type_traits>
 #include <array>
 
-static constexpr auto bpp = 8;
-static constexpr auto pitch = bpp * 8;
+template <typename T, size_t address> 
+class read_write
+{
+private:
+	static volatile inline T& ref = *reinterpret_cast<T*>(address);
+
+public:
+	constexpr static auto& Get() {
+		return ref;
+	}
+
+	constexpr static void Set(T const& val) {
+		ref = val;
+	}
+
+	template <typename U>
+	constexpr operator U() const {
+		return U{ref};
+	}
+
+	constexpr auto& operator=(T const& val) const {
+		ref = val;
+		return *this;
+	}
+
+	constexpr auto& operator |= (T const& val) const {
+		ref |= val;
+		return *this;
+	}
+};
+
+template <typename T, size_t address> 
+class write_only
+{
+private:
+	static volatile inline T& ref = *reinterpret_cast<T*>(address);
+
+public:
+	constexpr static void Set(T const& val) {
+		ref = val;
+	}
+
+	constexpr auto& operator=(T const& val) const {
+		ref = val;
+		return *this;
+	}
+};
 
 template <typename T>
 struct Bitfield
 {
-	constexpr operator T() const { return *(T*)this; }
+	constexpr T& raw() {
+		return *reinterpret_cast<T*>(this);
+	}
+
+	constexpr T const& raw() const {
+		return *reinterpret_cast<T const*>(this);
+	}
+
+	constexpr T volatile& raw() volatile {
+		return *reinterpret_cast<T volatile*>(this);
+	}
 };
 
-using CharEntry = TILE;
-
-struct ScreenEntry : public Bitfield<uint16_t>
+struct BgCnt : public Bitfield<uint16_t>
 {
-	uint16_t tileId  : 10;
-	uint16_t horFlip :  1;
-	uint16_t verFlip :  1;
-	uint16_t palBank :  4;
+	uint16_t priority : 2;
+	uint16_t charBlockBase : 2;
 
-	constexpr static ScreenEntry tile_id(uint16_t tile_id_) {
-		return ScreenEntry { .tileId = tile_id_ };
-	}
-	constexpr static ScreenEntry pal_bank(uint16_t pal_bank_) {
-		return ScreenEntry { .palBank = pal_bank_ };
-	}
+	uint16_t _ : 2;
 
+	uint16_t mosaicEnabled : 1;
+
+	enum class PalMode : uint16_t
+		{ Bits4 = 0
+		, Bits8 = 1
+
+		, Color16  = 0
+		, Color256 = 1
+	};
+	PalMode palMode : 1;
+
+	uint16_t screenBlockBase : 5;
+	uint16_t affineWrapEnabled : 1;
+
+	enum class MapSize : uint16_t
+		{ reg32x32 = 0
+		, reg64x32 = 1
+		, reg32x64 = 2
+		, reg64x64 = 3
+
+		, aff16x16 = 0
+		, aff32x32 = 1
+		, aff64x64 = 2
+		, aff128x128 = 3
+	};
+	MapSize mapSize : 2;
+
+	constexpr operator uint16_t() const {
+		return (priority          <<  0)
+			 | (charBlockBase     <<  2)
+			 | (mosaicEnabled     <<  6)
+			 | ((uint16_t)palMode <<  7)
+			 | (screenBlockBase   <<  8)
+			 | (affineWrapEnabled <<  13)
+			 | ((uint16_t)mapSize <<  14);
+	}
+} __attribute__((packed));
+
+struct ScreenEntry : Bitfield<uint16_t> {
+	uint16_t tileId   : 10;
+	uint16_t horzFlip :  1;
+	uint16_t vertFlip :  1;
+	uint16_t palBank  :  4;
+
+	constexpr operator uint16_t() const {
+		return (tileId   <<  0)
+			 | (horzFlip << 10)
+			 | (vertFlip << 11)
+			 | (palBank  << 12);
+	}
 } __attribute__((packed));
 static_assert(sizeof(ScreenEntry) == 2);
 
-static constexpr auto charEntries   = 0x4000 / sizeof(CharEntry);
-static constexpr auto screenEntries = 0x800 / sizeof(ScreenEntry);
+template <size_t address> 
+class screen_blocks {
+	using ScreenBlock = uint16_t[0x400];
+	static inline auto base = reinterpret_cast<ScreenBlock volatile*>(address);
 
-static constexpr auto screenBlockHeight = 32;
-static constexpr auto screenBlockWidth  = 32;
+public:
+	constexpr auto& operator[] (size_t i) const {
+		return base[i];
+	}
+};
 
-auto charBlocks    = tile_mem;
-auto screenBlocks  = se_mem;
+struct CharEntry {
+	uint32_t buf[8];
 
-static constexpr CharEntry tiles[] =
+public:
+	constexpr CharEntry(std::initializer_list<uint32_t> lines)
+		: buf{lines.begin()[0], lines.begin()[1], lines.begin()[2], lines.begin()[3], lines.begin()[4], lines.begin()[5], lines.begin()[6], lines.begin()[7]}
+	{}
+
+	auto& operator= (CharEntry const& rhs) volatile {
+		std::copy(rhs.buf, rhs.buf + 8, buf);
+		return *this;
+	}
+};
+
+template <size_t address> 
+class char_blocks {
+	using CharBlock = CharEntry[0x200];
+	static inline auto base = reinterpret_cast<CharBlock volatile*>(address);
+
+public:
+	constexpr auto& operator[] (size_t i) const {
+		return base[i];
+	}
+};
+
+struct Rgb15 {
+	constexpr static uint16_t make(uint16_t r, uint16_t g, uint16_t b) {
+		return ((r & 0x1f) << (5 * 0))
+		     | ((g & 0x1f) << (5 * 1))
+		     | ((b & 0x1f) << (5 * 2));
+	}
+};
+
+template <size_t address> 
+struct pal_banks {
+	using PalBank = uint16_t[0x10];
+	static inline auto base = reinterpret_cast<PalBank volatile*>(address);
+
+public:
+	constexpr auto& operator[] (size_t i) const {
+		return base[i];
+	}
+};
+
+static constexpr auto dispCntReg_addr = size_t{0x04000000};
+auto dispCntReg = read_write<uint16_t, dispCntReg_addr>{};
+
+static constexpr auto bg0CntReg_addr = size_t{0x04000008};
+auto bg0Cnt = read_write<uint16_t, bg0CntReg_addr>{};
+
+static constexpr auto bg0horzOffsReg_addr = size_t{0x04000010};
+auto bg0HorzOffsReg = write_only<uint32_t, bg0horzOffsReg_addr>{};
+
+static constexpr auto vidMem_addr = size_t{0x06000000};
+auto screenBlocks = screen_blocks<vidMem_addr>{};
+auto charBlocks   = char_blocks<vidMem_addr>{};
+
+static constexpr auto palMem_addr = size_t{0x05000000};
+auto palBanks = pal_banks<palMem_addr>{};
+
+static constexpr auto tiles = std::array<CharEntry, 2> {
 {
 	{ 0x11111111
 	, 0x01111111
@@ -57,138 +210,59 @@ static constexpr CharEntry tiles[] =
 	{ 0x00000000, 0x00100100, 0x01100110, 0x00011000
 	, 0x00011000, 0x01100110, 0x00100100, 0x00000000
 	}
-};
+} };
 
-struct Rgb15 : public Bitfield<uint16_t>
+static constexpr auto Red   = Rgb15::make(31,  0,  0);
+static constexpr auto Blue  = Rgb15::make( 0, 31,  0);
+static constexpr auto Green = Rgb15::make( 0,  0, 31);
+static constexpr auto Grey  = Rgb15::make(15, 15, 15);
+
+int main(int argc, char const* argv[])
 {
-	uint16_t _ : 1;
-	uint16_t r : 5;
-	uint16_t g : 5;
-	uint16_t b : 5;
+	dispCntReg = 0x1100;
 
-	constexpr Rgb15() : _{}, r{}, g{}, b{} {}
-	constexpr Rgb15(uint16_t r_, uint16_t g_, uint16_t b_) : _{}, r{r_}, g{g_}, b{b_} {}
-};
-static_assert(sizeof(Rgb15) == 2);
+	auto constexpr charBlockBase = 0;
+	auto constexpr screenBlockBase = 28;
 
-auto bgPalBanks = pal_bg_bank;
-
-struct BgControlReg : public Bitfield<uint16_t>
-{
-	uint16_t priority : 2;
-	uint16_t charBaseBlock : 2;
-
-	uint16_t _ : 2;
-
-	uint16_t mosaicEnabled : 1;
-
-	enum class ColorMode : uint16_t
-		{ FourBitsPerPixel  = 0
-		, EightBitsPerPixel = 1
+	bg0Cnt = BgCnt
+		{ .priority = 0
+		, .charBlockBase = charBlockBase
+		, .mosaicEnabled = false
+		, .palMode = BgCnt::PalMode::Bits4
+		, .screenBlockBase = screenBlockBase
+		, .affineWrapEnabled = false
+		, .mapSize = BgCnt::MapSize::reg64x64
 	};
 
-	ColorMode colorMode : 1;
+	auto offs = uint32_t{0x4};
+	bg0HorzOffsReg = offs;
+	offs++;
+	bg0HorzOffsReg = offs;
 
-	uint16_t screenBaseBlock : 5;
-	uint16_t affineWrapEnabled : 1;
+	palBanks[0][1] = Red;
+	palBanks[1][1] = Blue;
+	palBanks[2][1] = Green;
+	palBanks[3][1] = Grey;
 
-	enum class BgSize : uint16_t
-		{ Reg32x32 = 0
-		, Reg64x32 = 1
-		, Reg32x64 = 2
-		, Reg64x64 = 3
+	charBlocks[0][0] = tiles[0];
+	charBlocks[0][1] = tiles[1];
 
-		, Aff16x16 = 0
-		, Aff32x32 = 1
-		, Aff64x64 = 2
-		, Aff128x128 = 3
-	};
-
-	BgSize bgSize : 2;
-
-} __attribute__((packed));
-static_assert(sizeof(BgControlReg) == 2);
-
-struct Bg64x64
-{
-	BgControlReg volatile& bgControlReg;
-	uint32_t &horzOffs, &vertOffs;
-
-	static constexpr uint32_t charBaseBlock = 0;
-	static constexpr uint32_t screenBaseBlock = 28;
-
-	Bg64x64()
-		: bgControlReg{*(BgControlReg*)(0x04000008 + 2 * 0)} // REG_BG0CNT
-		, horzOffs{*(uint32_t*)(0x04000010 + 4 * 0)} // REG_BG0HOFS
-		, vertOffs{*(uint32_t*)(0x04000012 + 4 * 0)} // REG_BGVHOFS
-	{
-		bgControlReg.priority = 0;
-		bgControlReg.charBaseBlock = charBaseBlock;
-		bgControlReg.mosaicEnabled = false;
-		bgControlReg.colorMode = BgControlReg::ColorMode::FourBitsPerPixel;
-		bgControlReg.screenBaseBlock = screenBaseBlock;
-		bgControlReg.affineWrapEnabled = false;
-		bgControlReg.bgSize = BgControlReg::BgSize::Reg64x64;
-
-		horzOffs = 0;
-		vertOffs = 0;
-	}
-
-	constexpr auto charBlockIdx(auto tileId) const {
-		return charBaseBlock
-		    + (tileId / charEntries);
-	}
-
-	constexpr auto charEntryIdx(auto tileId) const {
-		return (tileId % charEntries);
-	}
-
-	constexpr auto screenBlockIdx(auto tx, auto ty) const {
-		return screenBaseBlock
-		     + (ty / screenBlockHeight) * (pitch / 32)
-		     + (tx / screenBlockWidth);
-	}
-
-	constexpr auto screenEntryIdx(auto tx, auto ty) const {
-		return (ty % screenBlockHeight) * screenBlockHeight
-		     + (tx % screenBlockWidth);
-	}
-};
-
-int main(int argc, char const* argv[]) {
-	REG_DISPCNT = DCNT_MODE0 | DCNT_BG0 | DCNT_OBJ;
-
-	auto bg0 = Bg64x64{};
-
-	bg0.horzOffs = 0;
-	bg0.vertOffs = 0;
-
-	auto const charBaseBlock = bg0.charBaseBlock;
-	charBlocks[charBaseBlock + 0][0] = tiles[0];
-	charBlocks[charBaseBlock + 0][1] = tiles[1];
-
-	bgPalBanks[0][1] = Rgb15{31,  0,  0};
-	bgPalBanks[1][1] = Rgb15{ 0, 31,  0};
-	bgPalBanks[2][1] = Rgb15{ 0,  0, 31};
-	bgPalBanks[3][1] = Rgb15{16, 16, 16};
-
-	auto const screenBaseBlock = bg0.screenBaseBlock;
-	for (int i = 0; i < 4; i++) {
-		for (int j = 0; j < screenBlockHeight * screenBlockWidth; j++) {
-			screenBlocks[screenBaseBlock + i][j] = ScreenEntry
-				{ .tileId  = 0
-				, .horFlip = 0
-				, .verFlip = 0
-				, .palBank = (uint16_t)i
-				};
+	for (uint32_t screenBlockOffs = 0; screenBlockOffs < 4; screenBlockOffs++) {
+		for (uint32_t screenEntryIdx = 0; screenEntryIdx < 32 * 32; screenEntryIdx++) {
+			screenBlocks[screenBlockBase + screenBlockOffs][screenEntryIdx] = ScreenEntry
+				{ .tileId = 0
+				, .horzFlip = 0
+				, .vertFlip = 0
+				, .palBank = (uint16_t)screenBlockOffs
+			};
 		}
 	}
 
-	auto const screenBlockIdx = bg0.screenBlockIdx(0, 0);
-	auto const screenEntryIdx = bg0.screenEntryIdx(0, 0);
-	screenBlocks[screenBlockIdx][screenEntryIdx] |= ScreenEntry::tile_id(1);
+	bg0Cnt |= BgCnt{ .affineWrapEnabled = true };
 
-	while (1) {}
+	screenBlocks[screenBlockBase][0] |= ScreenEntry{ .tileId = 0x1 };
+
+	while (true) {}
 
 	return 0;
 }
